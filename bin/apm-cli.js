09d57f4e98b626265a74be011bdd32120d822358
Kim T <kimturley@gmail.com>
2020-06-13 22:56:31 -0700

    Clean up old code/dependencies

--- bin/apm-cli.js
+++ bin/apm-cli.js
@@ -1,36 +1,14 @@
 #!/usr/bin/env node
 
-const chalk = require('chalk');
-const commander = require('commander');
+const program = require('commander');
 
 const apm = require('../lib/apm.js')
 const pkg = require('../package.json');
 
-function success(msg) {
-  console.log(`-------`);
-  console.log(chalk.green(msg));
-  process.exit(1);
-}
-
-function failure(msg) {
-  console.log(`-------`);
-  console.log(chalk.red(msg));
-  process.exit(0);
-}
-
-async function run(action) {
-  if (apm[action]) {
-    await apm[action]();
-    // success(`Command ${action} recognized`);
-  } else {
-    failure(`Error command not recognized`);
-  }
-  return true;
-}
-
-commander
+program
   .version(pkg.version)
-  .arguments('<action>')
-  .option('-t, --type <boolean>', 'Type')
-  .action(run)
-  .parse(process.argv);
+  .command('init')
+  .description('Set up a new or existing apm project.')
+  .action(apm.init);
+
+  program.parse(process.argv);
--- src/index.js
+++
@@ -1,5 +0,0 @@
-const extensions = require('./config/extensions.json');
-const file = require('./file.js');
-
-const library = file.searchDirectory('./example', extensions);
-console.log('library', JSON.stringify(library, null, 2));
