acd67ca121bad1a02b8087fd95126d6a6231b71a
Kim T <kimturley@gmail.com>
2020-09-20 18:32:25 -0700

    Generate separate plugin zip files

--- bin/studiorack-cli.js
+++ bin/studiorack-cli.js
@@ -44,6 +44,7 @@ program
 program
   .command('validate [path]')
   .option('-m, --metadata', 'return metadata only as json')
+  .option('-z, --zip', 'create a zip file of plugin')
   .description('Validate a plugin using the Steinberg VST3 SDK validator')
   .action(studiorack.validate);
 
