e7c9fd419d5c5559ce4eaa67ef0fbc1d0482242c
Kim T <kimturley@gmail.com>
2022-05-20 21:33:44 -0700

    Example cli test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,4 +1,30 @@
+import { exec, ExecException } from 'child_process';
+import path from 'path';
 
-test('Example test', () => {
-  expect(1+2).toEqual(3);
+interface CliOutput {
+  code: number;
+  error: ExecException | null;
+  stdout: string;
+  stderr: string;
+}
+
+const PLUGIN_DIR: string = path.join('test', 'plugins');
+
+test('Example test', async () => {
+  const output: CliOutput = await cli(`studiorack test "${PLUGIN_DIR}"`);
+  expect(output.stdout).toEqual(`⎋ /Users/kimturley/Library/Preferences/studiorack/config.json
+`);
 });
+
+function cli(cmd: string, cwd = '.'): Promise<CliOutput> {
+  return new Promise(resolve => {
+    exec(cmd, { cwd }, (error, stdout, stderr) => {
+      resolve({
+        code: error && error.code ? error.code : 0,
+        error,
+        stdout,
+        stderr
+      });
+    });
+  });
+};
