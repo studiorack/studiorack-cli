81f8023ed1f5e5e24623c5a5a81385f9864211af
Kim T <kimturley@gmail.com>
2022-07-12 21:44:08 -0700

    Upgrade to new core library and swap test command for run

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { Command } from 'commander';
 import { config } from './config';
 import { plugin } from './plugin';
 import { project } from './project';
-import { testFolder, validateFolder } from '@studiorack/core';
+import { PluginLocal, pluginValidateFolder, PluginValidationOptions, toolFolder, toolInstall } from '@studiorack/core';
 
 const pkg = require('../package.json');
 const program = new Command();
@@ -13,13 +13,16 @@ program.addCommand(plugin);
 program.addCommand(project);
 
 program
-  .command('test [path]')
-  .option('-s, --summary', 'plugin test summary json file')
-  .description('Test a plugin using the Tracktion plugin validator')
+  .command('run [path]')
+  .option('-t, --tool <type>', 'tool (clapinfo, pluginval or validator)')
+  .description('Run a command-line tool')
   .action(async (pluginPath: string, options?: any) => {
-    const result = await testFolder(pluginPath, options);
-    if (options.summary) {
-      console.log(result);
+    await toolInstall(options.tool);
+    const results: string[] = toolFolder(options.tool, pluginPath);
+    if (results.length) {
+      results.forEach((result: string) => {
+        console.log(result);
+      });
     }
   });
 
@@ -32,8 +35,8 @@ program
   .option('-t, --txt', 'plugin txt file')
   .option('-z, --zip', 'create a zip file of plugin')
   .description('Validate a plugin using the Steinberg VST3 SDK validator')
-  .action(async (pluginPath: string, options?: any) => {
-    const result = await validateFolder(pluginPath, options);
+  .action(async (pluginPath: string, options: PluginValidationOptions) => {
+    const result: PluginLocal[] = await pluginValidateFolder(pluginPath, options);
     if (options.summary) {
       console.log(result);
     }
