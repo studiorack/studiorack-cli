c1e50c7bb5c245eedcba0f9734bd0fcbcaeb2999
Kim T <kimturley@gmail.com>
2024-10-14 21:26:47 -0700

    Update eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,18 @@
 import globals from 'globals';
-import pluginJs from '@eslint/js';
+import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default [
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
+export default tseslint.config(
+  eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-    },
+    ignores: ['build'],
   },
   {
-    ignores: ['src/convert.ts'],
+    languageOptions: { globals: globals.node },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+    },
   },
-];
+);
